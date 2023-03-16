fd11135b6a7a98b9795bd1e0eaf15b32841d089c
Dirk Arnez <smalldirkalex@gmail.com>
2023-03-17 02:35:12 +0800

    - update upload-file-to-github.cmake

--- upload-file-to-github.cmake
+++ upload-file-to-github.cmake
@@ -1,1 +1,15 @@
-base64 somefile.txt | base64 -d - > 123.txt
+# base64 somefile.txt | base64 -d - > 123.txt
+
+
+echo "{\"message\":\"my commit message\",\"committer\":{\"name\":\"Monalisa Octocat\",\"email\":\"octocat@github.com\"},\"content\":\"$(base64 ./20201014180756916.png)\"}" > temp.json
+
+curl -L \
+  -X PUT \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${XXXXXX}"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/dirkarnez/gittest/contents/20201014180756916.png \
+  -d @./temp.json && \
+rm ./temp.json
+
+read -p "jajaj"

31b4c9539dc506307c1aca9f65f12db2fcff1668
dirkarnez <smalldirkalex@gmail.com>
2023-03-21 22:35:55 +0800

    - minor updates

--- git-clone-existing.cmake
+++ git-clone-existing.cmake
@@ -1,13 +1,13 @@
 include(${CMAKE_CURRENT_SOURCE_DIR}/common/secrets.cmake)
 
-if(NOT DEFINED GIT_TOKEN)
-    message(FATAL "???")
+if(NOT DEFINED GIT_TOKEN OR "${GIT_TOKEN}" STREQUAL "")
+    message(FATAL_ERROR "GIT_TOKEN not available")
     # generate one
 endif()
 
 include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
-if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
-    message(FATAL "???")
+if (UTILS_IS_FOUND STREQUAL "utils.cmake not available")
+    message(FATAL_ERROR "???")
     # generate one
 endif()
 
@@ -19,4 +19,4 @@ execute_process(
     COMMAND git 
     "clone" 
     "https://dirkarnez:${GIT_TOKEN}@github.com/dirkarnez/${REPO_NAME}.git"
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+    WORKING_DIRECTORY $ENV{USERPROFILE}/Downloads)

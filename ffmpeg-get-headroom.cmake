0f5865fd26ea6f7396d329659421b59f5dd0627a
dirkarnez <smalldirkalex@gmail.com>
2023-05-28 20:01:23 +0800

    - add ffmpeg-get-headroom.cmake, ffmpeg-normalize-to-headroom.cmake

---
+++ ffmpeg-get-headroom.cmake
@@ -0,0 +1,17 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+message("file")
+input(file)
+
+# file(TO_CMAKE_PATH "${DIRECTORY}" DIRECTORY_NORMALIZED)
+# message(${file})
+
+execute_process(
+	COMMAND ffmpeg -i "${file}" -af "volumedetect" -vn -sn -dn -f null NUL
+	WORKING_DIRECTORY $ENV{USERPROFILE}/Downloads
+	OUTPUT_STRIP_TRAILING_WHITESPACE)
+
---
+++ ffmpeg-normalize-to-headroom.cmake
@@ -0,0 +1,18 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+message("file")
+input(file)
+message(${file})
+
+message("DB (eg 0.5)")
+input(DB)
+message(${DB})
+
+execute_process(
+	COMMAND ffmpeg -i "${file}" -af "volume=volume=${DB}dB:precision=fixed" output.wav
+	WORKING_DIRECTORY $ENV{USERPROFILE}/Downloads
+	OUTPUT_STRIP_TRAILING_WHITESPACE)

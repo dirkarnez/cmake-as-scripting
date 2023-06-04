f6aa39d0f13e14d1e519a0cdac7ad6bc91d99241
dirkarnez <smalldirkalex@gmail.com>
2023-06-04 17:37:41 +0800

    - complete tar.cmake

--- tar.cmake
+++ tar.cmake
@@ -1,5 +1,32 @@
-# tar -zxvf xxx.tar.gz
-# tar -jxvf xxx.tar.bz2
-# tar -xf archive.tar.xz
-# -z gzip
-# -j bzip2
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+message("file")
+input(file)
+
+message(${file})
+
+
+if(${file} MATCHES ".*.tar.gz$")
+    # -z gzip
+    execute_process(
+        COMMAND  tar -zxvf ${file}
+        WORKING_DIRECTORY $ENV{USERPROFILE}/Downloads
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+elseif(${file} MATCHES ".*.tar.bz2$")
+    # -j bzip2
+    execute_process(
+        COMMAND  tar -jxvf ${file}
+        WORKING_DIRECTORY $ENV{USERPROFILE}/Downloads
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+elseif(${file} MATCHES ".*.tar.xz$")
+    execute_process(
+        COMMAND  tar -xf ${file}
+        WORKING_DIRECTORY $ENV{USERPROFILE}/Downloads
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+else() 
+    message("Not support yet")
+endif()

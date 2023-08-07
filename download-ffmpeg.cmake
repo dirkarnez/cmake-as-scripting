c3a5c4ad7c0bbf432b8c3bcd83b7cae932b3293e
Dirk Arnez <smalldirkalex@gmail.com>
2023-08-07 13:19:44 +0800

    - add download-ffmpeg.cmake

---
+++ download-ffmpeg.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file("https://github.com/GyanD/codexffmpeg/releases/download/6.0/ffmpeg-6.0-full_build-shared.zip")

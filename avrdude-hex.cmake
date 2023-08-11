7ad3a8553c54de373ee9f4b18c2ac7fdf60a9df1
Dirk Arnez <smalldirkalex@gmail.com>
2023-08-12 01:16:40 +0800

    - add avrdude-hex.cmake

---
+++ avrdude-hex.cmake
@@ -0,0 +1,2 @@
+"C:\Users\stude\Downloads\arduino-1.8.19-windows\arduino-1.8.19\hardware\tools\avr\bin\avrdude.exe" -C"C:\Users\stude\Downloads\arduino-1.8.19-windows\arduino-1.8.19\hardware\tools\avr\etc\avrdude.conf" -v -v -v -patmega328p -carduino -P\\.\COM9 -b115200 -D -Uflash:w:"AssemblerApplication1.hex":i
+pause

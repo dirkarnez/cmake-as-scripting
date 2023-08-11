9b214f1c1a7424a69480ec66a33e5d4d4a5305b8
dirkarnez <smalldirkalex@gmail.com>
2023-08-11 19:20:43 +0800

    - add start-destreamer.cmake

---
+++ start-destreamer.cmake.cmake
@@ -0,0 +1,16 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+message("account (with @connect.polyu.hk): ")
+input(account)
+message("${account}")
+
+message("input file")
+input(inputFile)
+
+execute_process(
+	COMMAND node.exe --max-http-header-size 32768 .\build\src\destreamer.js -u "${account}" --inputFile "${inputFile}"
+	WORKING_DIRECTORY $ENV{USERPROFILE}/Downloads/destreamer)

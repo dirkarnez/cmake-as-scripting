7186d27dadf6c4fa78dc4b9c0185095750d86293
dirkarnez <smalldirkalex@gmail.com>
2023-08-19 10:58:15 +0800

    - fix common/env.cmake

--- common/env.cmake
+++ common/env.cmake
@@ -12,7 +12,7 @@ message("${CURRENT_USER} ${DOMAIN}")
 
 if("${DOMAIN}" STREQUAL "hh\\")
 	message("in school")
-	
+
 	set(DOWNLOADS_DIR "$ENV{USERPROFILE}\\Downloads")
 	set(ENV{GOROOT} "${DOWNLOADS_DIR}\\go1.21.0.windows-amd64\\go")
 	set(ENV{GOPATH} "P:\\Downloads\\gopath")
@@ -21,11 +21,17 @@ if("${DOMAIN}" STREQUAL "hh\\")
 	set(ENV{VSCODE_USER_DATA_PATH} "P:\\Downloads\\vscode-data\\user-data")
 
 	set(ENV{PATH} "$ENV{PATH};${DOWNLOADS_DIR}")
-	set(ENV{PATH} "$ENV{PATH};${GOROOT}\\bin")
+	set(ENV{PATH} "$ENV{PATH};$ENV{GOROOT}\\bin")
 	set(ENV{PATH} "$ENV{PATH};${DOWNLOADS_DIR}\\PortableGit\\bin")
 	set(ENV{PATH} "$ENV{PATH};${DOWNLOADS_DIR}\\x86_64-8.1.0-release-posix-seh-rt_v6-rev0")
 	set(ENV{PATH} "$ENV{PATH};${DOWNLOADS_DIR}\\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\\bin")
 	set(ENV{PATH} "$ENV{PATH};${DOWNLOADS_DIR}\\cmake-3.22.2-windows-x86_64\\bin")
+
+	message("${DOWNLOADS_DIR}")
+	message("$ENV{GOROOT}")
+	message("$ENV{PATH}")
+	message("$ENV{VSCODE_EXTENSION_PATH}")
+	message("$ENV{VSCODE_USER_DATA_PATH}")
 endif()
 
 
--- start-vscode-go1.21.0.cmake
+++ start-vscode-go1.21.0.cmake
@@ -1,22 +1,4 @@
-set(DOWNLOADS_DIR "$ENV{USERPROFILE}\\Downloads")
-set(ENV{GOROOT} "${DOWNLOADS_DIR}\\go1.21.0.windows-amd64\\go")
-set(ENV{GOPATH} "P:\\Downloads\\gopath")
-
-set(ENV{VSCODE_EXTENSION_PATH} "P:\\Downloads\\vscode-data\\extensions")
-set(ENV{VSCODE_USER_DATA_PATH} "P:\\Downloads\\vscode-data\\user-data")
-
-set(ENV{PATH} "$ENV{PATH};${DOWNLOADS_DIR}")
-set(ENV{PATH} "$ENV{PATH};$ENV{GOROOT}\\bin")
-set(ENV{PATH} "$ENV{PATH};${DOWNLOADS_DIR}\\PortableGit\\bin")
-set(ENV{PATH} "$ENV{PATH};${DOWNLOADS_DIR}\\x86_64-8.1.0-release-posix-seh-rt_v6-rev0")
-set(ENV{PATH} "$ENV{PATH};${DOWNLOADS_DIR}\\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\\bin")
-set(ENV{PATH} "$ENV{PATH};${DOWNLOADS_DIR}\\cmake-3.22.2-windows-x86_64\\bin")
-
-message("${DOWNLOADS_DIR}")
-message("$ENV{GOROOT}")
-message("$ENV{PATH}")
-message("$ENV{VSCODE_EXTENSION_PATH}")
-message("$ENV{VSCODE_USER_DATA_PATH}")
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/env.cmake)
 
 if(NOT EXISTS $ENV{VSCODE_EXTENSION_PATH})
     # Create the directory

29573c16e64e309b0f3416eb66944394c14b7a9a
dirkarnez <smalldirkalex@gmail.com>
2023-08-20 14:18:31 +0800

    - update start-vscode-go1.21.0.cmake

--- start-vscode-go1.21.0.cmake
+++ start-vscode-go1.21.0.cmake
@@ -1,17 +1,20 @@
 include(${CMAKE_CURRENT_SOURCE_DIR}/common/env.cmake)
-
-if(NOT EXISTS $ENV{VSCODE_EXTENSION_PATH})
-    # Create the directory
-    file(MAKE_DIRECTORY $ENV{VSCODE_EXTENSION_PATH})
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
 endif()
 
-if(NOT EXISTS $ENV{VSCODE_USER_DATA_PATH})
-    # Create the directory
-    file(MAKE_DIRECTORY $ENV{VSCODE_USER_DATA_PATH})
+set(ENV{GOROOT} "${DOWNLOADS_DIR}\\go1.21.0.windows-amd64\\go")
+if(NOT EXISTS $ENV{GOROOT})
+	download_file("https://go.dev/dl/go1.21.0.windows-amd64.zip")
 endif()
+
+set(ENV{PATH} "$ENV{PATH};$ENV{GOROOT}\\bin")
 
 execute_process(
 	COMMAND "${DOWNLOADS_DIR}\\VSCode-win32-x64-1.66.1\\Code.exe" 
 	--extensions-dir "$ENV{VSCODE_EXTENSION_PATH}" 
 	--user-data-dir "$ENV{VSCODE_USER_DATA_PATH}"
+	"."
 	WORKING_DIRECTORY ${DOWNLOADS_DIR})

8c124285bade58c9d3eaab6e36642cea817141ce
dirkarnez <smalldirkalex@gmail.com>
2023-08-20 14:17:43 +0800

    - update common/env.cmake

--- common/env.cmake
+++ common/env.cmake
@@ -14,24 +14,35 @@ if("${DOMAIN}" STREQUAL "hh\\")
 	message("in school")
 
 	set(DOWNLOADS_DIR "$ENV{USERPROFILE}\\Downloads")
-	set(ENV{GOROOT} "${DOWNLOADS_DIR}\\go1.21.0.windows-amd64\\go")
 	set(ENV{GOPATH} "P:\\Downloads\\gopath")
-
-	set(ENV{VSCODE_EXTENSION_PATH} "P:\\Downloads\\vscode-data\\extensions")
-	set(ENV{VSCODE_USER_DATA_PATH} "P:\\Downloads\\vscode-data\\user-data")
-
+	
 	set(ENV{PATH} "$ENV{PATH};${DOWNLOADS_DIR}")
-	set(ENV{PATH} "$ENV{PATH};$ENV{GOROOT}\\bin")
 	set(ENV{PATH} "$ENV{PATH};${DOWNLOADS_DIR}\\PortableGit\\bin")
 	set(ENV{PATH} "$ENV{PATH};${DOWNLOADS_DIR}\\x86_64-8.1.0-release-posix-seh-rt_v6-rev0")
 	set(ENV{PATH} "$ENV{PATH};${DOWNLOADS_DIR}\\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\\bin")
 	set(ENV{PATH} "$ENV{PATH};${DOWNLOADS_DIR}\\cmake-3.22.2-windows-x86_64\\bin")
+	
+	set(ENV{VSCODE_EXTENSION_PATH} "P:\\Downloads\\vscode-data\\extensions")
+	set(ENV{VSCODE_USER_DATA_PATH} "P:\\Downloads\\vscode-data\\user-data")
+	
+	if(NOT EXISTS $ENV{VSCODE_EXTENSION_PATH})
+		# Create the directory
+		file(MAKE_DIRECTORY $ENV{VSCODE_EXTENSION_PATH})
+	endif()
+
+	if(NOT EXISTS $ENV{VSCODE_USER_DATA_PATH})
+		# Create the directory
+		file(MAKE_DIRECTORY $ENV{VSCODE_USER_DATA_PATH})
+	endif()
+	
 
 	message("${DOWNLOADS_DIR}")
 	message("$ENV{GOROOT}")
 	message("$ENV{PATH}")
 	message("$ENV{VSCODE_EXTENSION_PATH}")
 	message("$ENV{VSCODE_USER_DATA_PATH}")
+	
+	
 endif()
 
 

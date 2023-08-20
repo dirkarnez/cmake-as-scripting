5466644cd0292ee1c616078abe9f258b22b8fcff
dirkarnez <smalldirkalex@gmail.com>
2023-08-20 14:18:19 +0800

    - add start-vscode-go1.17.5.cmake

---
+++ start-vscode-go1.17.5.cmake
@@ -0,0 +1,20 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/env.cmake)
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+set(ENV{GOROOT} "${DOWNLOADS_DIR}\\go1.17.5.windows-amd64\\go")
+if(NOT EXISTS $ENV{GOROOT})
+	download_file("https://go.dev/dl/go1.17.5.windows-amd64.zip")
+endif()
+
+set(ENV{PATH} "$ENV{PATH};$ENV{GOROOT}\\bin")
+
+execute_process(
+	COMMAND "${DOWNLOADS_DIR}\\VSCode-win32-x64-1.66.1\\Code.exe" 
+	--extensions-dir "$ENV{VSCODE_EXTENSION_PATH}" 
+	--user-data-dir "$ENV{VSCODE_USER_DATA_PATH}"
+	"."
+	WORKING_DIRECTORY ${DOWNLOADS_DIR})

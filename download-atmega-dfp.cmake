f0ab249aba9dfa7f2e3c52e9862f72f765fa120b
dirkarnez <smalldirkalex@gmail.com>
2023-09-03 18:09:08 +0800

    - add download-atmega-dfp.cmake

---
+++ download-atmega-dfp.cmake
@@ -0,0 +1,11 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+# search "ATmega_DFP" on http://packs.download.atmel.com/ 
+download_file("http://packs.download.atmel.com/Atmel.ATmega_DFP.2.1.506.atpack")
+
+
+

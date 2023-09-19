21afdd28627aa2a1cf36e0eb2415042238d3f02d
dirkarnez <smalldirkalex@gmail.com>
2023-09-20 00:42:02 +0800

    - update common/git.cmake

--- common/git.cmake
+++ common/git.cmake
@@ -1,2 +1,6 @@
 execute_process(
 	COMMAND git config --global credential.helper WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+execute_process(
+	COMMAND git config --global user.name "dirkarnez" WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+execute_process(
+	COMMAND git config --global user.email "smalldirkalex@gmail.com" WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})

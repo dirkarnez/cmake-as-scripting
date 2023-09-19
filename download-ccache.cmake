c0c2e7a9abf8420cb89fe95f1452ca92a660b660
dirkarnez <smalldirkalex@gmail.com>
2023-09-19 22:51:47 +0800

    - add download-ccache.cmake

---
+++ download-ccache.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file_and_uncompress("https://github.com/ccache/ccache/releases/download/v4.8.3/ccache-4.8.3-windows-x86_64.zip")

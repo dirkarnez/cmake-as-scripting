411a7759c6ed215d0f0d705a5f95036268fedaaf
Dirk Arnez <smalldirkalex@gmail.com>
2023-10-01 19:48:37 +0800

    - add download-openscad-2021.01.cmake

---
+++ download-openscad-2021.01.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file_and_uncompress("https://files.openscad.org/OpenSCAD-2021.01-x86-64.zip")

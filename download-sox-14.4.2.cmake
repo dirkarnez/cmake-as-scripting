3f03b3c8ad81fff5ecb40861fec6783245e8a1cf
dirkarnez <smalldirkalex@gmail.com>
2023-10-04 22:56:48 +0800

    - add download-sox-14.4.2.cmake

---
+++ download-sox-14.4.2.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file_and_uncompress("https://zenlayer.dl.sourceforge.net/project/sox/sox/14.4.2/sox-14.4.2-win32.zip")

7a99fa66eb1073cd45ed50a882df99015548d3f5
dirkarnez <smalldirkalex@gmail.com>
2023-10-04 22:57:20 +0800

    - add download-vbindiff-3.0-beta5.cmake

---
+++ download-vbindiff-3.0-beta5.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file_and_uncompress("https://www.cjmweb.net/vbindiff/VBinDiff-3.0_beta5.zip")

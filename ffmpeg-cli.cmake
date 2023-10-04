dfdd17d78efbb9b55199dbd6e677ecdf3d7f1021
dirkarnez <smalldirkalex@gmail.com>
2023-10-04 22:57:09 +0800

    - add ffmpeg-cli.cmake

---
+++ ffmpeg-cli.cmake
@@ -0,0 +1,15 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/ffmpeg.cmake OPTIONAL RESULT_VARIABLE FFMPEG_IS_FOUND)
+if (FFMPEG_IS_FOUND STREQUAL "NOTFOUND")
+	message("???")
+	# generate one
+endif()
+
+execute_process(
+    COMMAND cmd
+    WORKING_DIRECTORY $ENV{USERPROFILE}/Downloads)

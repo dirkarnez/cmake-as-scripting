d08ec5a3f0dcbf554b619ceb9b9295b017a385a2
dirkarnez <smalldirkalex@gmail.com>
2023-10-11 15:12:29 +0800

    - update download-xmachoviewer-0.04.cmake

--- download-xmachoviewer-0.04.cmake
+++ download-xmachoviewer-0.04.cmake
@@ -4,4 +4,4 @@ if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
     # generate one
 endif()
 
-download_file_and_run("https://github.com/horsicq/XMachOViewer/releases/download/0.04/xmachoviewer_win64_portable_0.04.zip")
+download_file_and_uncompress("https://github.com/horsicq/XMachOViewer/releases/download/0.04/xmachoviewer_win64_portable_0.04.zip")

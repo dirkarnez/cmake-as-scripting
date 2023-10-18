4124850e4e236809c2309597d6d6aba8b57ffac5
dirkarnez <smalldirkalex@gmail.com>
2023-10-18 11:25:21 +0800

    - wip

--- common/download-vscode-latest-version.bat
+++ common/download-vscode-latest-version.bat
@@ -10,6 +10,6 @@ for /f "tokens=5 delims=/" %%i in ("%myvar%") do (
   set fileName=%%i
 )
 
-@REM C:\Windows\System32\curl.exe -s "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-archive" -L --output %fileName%
+C:\Windows\System32\curl.exe -s "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-archive" -L --output %fileName%
 echo %fileName%
 endlocal 

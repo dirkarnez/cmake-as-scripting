3ec41ea210dca11e258bdfa94abb8cf2c6ee1ac1
dirkarnez <smalldirkalex@gmail.com>
2023-10-22 00:16:59 +0800

    - update common/apps.cmake

--- common/apps.cmake
+++ common/apps.cmake
@@ -18,12 +18,6 @@ if(NOT EXISTS "$ENV{USERPROFILE}/Downloads/${FILE_NAME_WITHOUT_EXTENSION}/Code.e
 	string(REGEX MATCH https://.*.zip REDIRECTED_URL ${REDIRECTED_INFORMATION})
 	message("latest version vscode not found, downloading... ${REDIRECTED_URL}")
 	download_file_and_uncompress("${REDIRECTED_URL}")
-	
-	execute_process(
-		COMMAND curl -s https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-archive
-		OUTPUT_VARIABLE REDIRECTED_INFORMATION
-		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-		OUTPUT_STRIP_TRAILING_WHITESPACE)
 else()
 	message("latest version vscode found")
 endif()

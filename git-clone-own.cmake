78f9da6d61523d672bc408e32c2bd56c961cd8da
dirkarnez <smalldirkalex@gmail.com>
2023-11-28 23:36:35 +0800

    - update git-clone-own.cmake

--- git-clone-own.cmake
+++ git-clone-own.cmake
@@ -29,8 +29,29 @@ if(NOT EXISTS "$ENV{USERPROFILE}/Downloads/${REPO_NAME}")
 		WORKING_DIRECTORY "$ENV{USERPROFILE}/Downloads")
 endif()
 
- 
-if(EXISTS "$ENV{USERPROFILE}/Downloads/${REPO_NAME}/go.mod" OR EXISTS "$ENV{USERPROFILE}/Downloads/${REPO_NAME}/main.go")    
+set(START_VSCODE FALSE)
+
+function(check_is_keil_mdk_project RETURN_VARIABLE)
+	file(TO_CMAKE_PATH "$ENV{USERPROFILE}/Downloads/${REPO_NAME}/" DIRECTORY_NORMALIZED)
+
+	file(GLOB ALL_OF_MY_FILES LIST_DIRECTORIES false ${DIRECTORY_NORMALIZED}/*)
+
+	foreach(file ${ALL_OF_MY_FILES})
+		get_filename_component(FILE_EXTENSION ${file} LAST_EXT)
+		if (FILE_EXTENSION STREQUAL ".uvprojx")
+			SET(${RETURN_VARIABLE} TRUE PARENT_SCOPE)
+		endif()
+	endforeach()
+endfunction()
+
+
+check_is_keil_mdk_project(IS_KEIL_MDK_PROJECT)
+
+if(IS_KEIL_MDK_PROJECT)
+	message(STATUS "Keil MDK project")
+	
+	set(START_VSCODE FALSE)
+elseif(EXISTS "$ENV{USERPROFILE}/Downloads/${REPO_NAME}/go.mod" OR EXISTS "$ENV{USERPROFILE}/Downloads/${REPO_NAME}/main.go")    
     message(STATUS "Golang project")
 	
 	include(${CMAKE_CURRENT_SOURCE_DIR}/common/go.cmake OPTIONAL RESULT_VARIABLE GO_IS_FOUND)
@@ -39,6 +60,8 @@ if(EXISTS "$ENV{USERPROFILE}/Downloads/${REPO_NAME}/go.mod" OR EXISTS "$ENV{USER
 	endif()
 	
 	setup_go("1.21.0")
+	
+	set(START_VSCODE TRUE)
 elseif(EXISTS "$ENV{USERPROFILE}/Downloads/${REPO_NAME}/package.json" OR EXISTS "$ENV{USERPROFILE}/Downloads/${REPO_NAME}/index.js" OR EXISTS "$ENV{USERPROFILE}/Downloads/${REPO_NAME}/main.js")
     message(STATUS "Node.js project")
 	
@@ -48,8 +71,12 @@ elseif(EXISTS "$ENV{USERPROFILE}/Downloads/${REPO_NAME}/package.json" OR EXISTS 
 	endif()
 
 	setup_node("18.18.0")
+	
+	set(START_VSCODE TRUE)
 elseif(EXISTS "$ENV{USERPROFILE}/Downloads/${REPO_NAME}/CMakeLists.txt")
     message(STATUS "CMake project")
+	
+	set(START_VSCODE TRUE)
 elseif(EXISTS "$ENV{USERPROFILE}/Downloads/${REPO_NAME}/requirements.txt")
     message(STATUS "Python project")
 	
@@ -59,23 +86,34 @@ elseif(EXISTS "$ENV{USERPROFILE}/Downloads/${REPO_NAME}/requirements.txt")
 	endif()
 
 	setup_python("3.10.8")
+	
+	set(START_VSCODE TRUE)
 elseif(EXISTS "$ENV{USERPROFILE}/Downloads/${REPO_NAME}/main.tex")
     message(STATUS "Tex project")
 	
 	set(ENV{PATH} "$ENV{PATH};$ENV{USERPROFILE}\\Downloads\\miktex-portable-v5.5.0.1763023\\texmfs\\install\\miktex\\bin\\x64;$ENV{USERPROFILE}\\Downloads\\strawberry-perl-5.38.0.1-64bit-portable\\perl\\bin")
 	# set(ENV{PATH} "$ENV{PATH};$ENV{USERPROFILE}\\Downloads\\texlive-portable-20231107\\2023\\bin\\windows")
+	
+	set(START_VSCODE TRUE)
 elseif(EXISTS "$ENV{USERPROFILE}/Downloads/${REPO_NAME}/index.html")
     message(STATUS "HTML project")
-endif()
-
-include(${CMAKE_CURRENT_SOURCE_DIR}/common/apps.cmake OPTIONAL RESULT_VARIABLE APPS_IS_FOUND)
-if (APPS_IS_FOUND STREQUAL "NOTFOUND")
-    message(FATAL_ERROR "APPS_IS_FOUND: ${APPS_IS_FOUND}")
-endif()
-
 	
-execute_process(
-	COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/common/exec-detached.bat 
-	${VSCODE}
-	"."
-	WORKING_DIRECTORY "$ENV{USERPROFILE}\\Downloads\\${REPO_NAME}")
+	set(START_VSCODE TRUE)
+else()
+	message(STATUS "Unknown")
+endif()
+
+
+if (START_VSCODE)
+	include(${CMAKE_CURRENT_SOURCE_DIR}/common/apps.cmake OPTIONAL RESULT_VARIABLE APPS_IS_FOUND)
+	if (APPS_IS_FOUND STREQUAL "NOTFOUND")
+		message(FATAL_ERROR "APPS_IS_FOUND: ${APPS_IS_FOUND}")
+	endif()
+
+		
+	execute_process(
+		COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/common/exec-detached.bat 
+		${VSCODE}
+		"."
+		WORKING_DIRECTORY "$ENV{USERPROFILE}\\Downloads\\${REPO_NAME}")
+endif()

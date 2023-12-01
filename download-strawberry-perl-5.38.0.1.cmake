40245053727822fa7f93b8e5d7f501d3fe99eb06
dirkarnez <smalldirkalex@gmail.com>
2023-12-01 23:53:54 +0800

    - add download-strawberry-perl-5.38.0.1.cmake

---
+++ download-strawberry-perl-5.38.0.1.cmake
@@ -0,0 +1,10 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/secrets.cmake)
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+if(NOT EXISTS "$ENV{USERPROFILE}/Downloads/strawberry-perl-5.38.0.1-64bit-portable/portableshell.bat")
+	download_file_and_uncompress("https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_5380_5361/strawberry-perl-5.38.0.1-64bit-portable.zip")
+endif()

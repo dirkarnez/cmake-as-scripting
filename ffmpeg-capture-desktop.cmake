35ff7bccf81d6d7b802de0585c4921347463f698
Dirk Arnez <smalldirkalex@gmail.com>
2024-01-18 03:23:29 +0800

    - update ffmpeg-capture-desktop.cmake

--- ffmpeg-capture-desktop.cmake
+++ ffmpeg-capture-desktop.cmake
@@ -1,2 +1,3 @@
 start "" /B "%USERPROFILE%\Downloads\ffmpeg-6.0-full_build-shared\ffmpeg-6.0-full_build-shared\bin\ffmpeg.exe" -f gdigrab -framerate 30 -i desktop -c:v libx264 output.mp4
 # https://trac.ffmpeg.org/wiki/Capture/Desktop
+# https://github.com/hansalemaos/ffmpeg_screenshot_pipe

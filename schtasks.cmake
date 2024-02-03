332886d6cca3b23c467ed82cf7aee8804f1051d3
Dirk Arnez <smalldirkalex@gmail.com>
2024-02-03 19:33:34 +0800

    - update schtasks.cmake

--- schtasks.cmake
+++ schtasks.cmake
@@ -1,1 +1,3 @@
 schtasks /create /tn mytask /tr "cmd /c 'start taskmgr && start schtasks /delete /tn mytask /f'" /sc once /st 20:17 /sd 06/05/2023
+@REM beep
+schtasks /create /tn mytask /tr "cmd /c 'start rundll32 user32.dll,MessageBeep && start schtasks /delete /tn mytask /f'" /sc once /st 19:34 /sd 03/02/2024

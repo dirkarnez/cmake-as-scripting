d037de012d3c7316d7bc50d5bf0c2945fe42a358
dirkarnez <smalldirkalex@gmail.com>
2024-02-08 20:43:55 +0800

    - upload files

--- ffmpeg-capture-desktop.cmake
+++ ffmpeg-capture-desktop.cmake
@@ -1,3 +1,34 @@
-start "" /B "%USERPROFILE%\Downloads\ffmpeg-6.0-full_build-shared\ffmpeg-6.0-full_build-shared\bin\ffmpeg.exe" -f gdigrab -framerate 30 -i desktop -c:v libx264 output.mp4
 # https://trac.ffmpeg.org/wiki/Capture/Desktop
 # https://github.com/hansalemaos/ffmpeg_screenshot_pipe
+
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/ffmpeg.cmake OPTIONAL RESULT_VARIABLE FFMPEG_IS_FOUND)
+if (FFMPEG_IS_FOUND STREQUAL "NOTFOUND")
+	message("???")
+	# generate one
+endif()
+
+message("file")
+input(file)
+
+# file(TO_CMAKE_PATH "${DIRECTORY}" DIRECTORY_NORMALIZED)
+# message(${file})
+
+get_filename_component(FILE_NAME_WITHOUT_EXTENSION ${file} NAME_WLE)
+
+# This command has no audio recorded
+execute_process(
+	COMMAND ffmpeg -f gdigrab -framerate 30 -i desktop -c:v libx264 "${file}.mp4"
+	WORKING_DIRECTORY $ENV{USERPROFILE}/Downloads
+	OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+
+
+

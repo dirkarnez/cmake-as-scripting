62d0f200c2558461dfc35b309c14ad0ca3b53407
dirkarnez <smalldirkalex@gmail.com>
2024-02-14 22:50:48 +0800

    - add imagemagick-jpeg-compress.cmake, wip

---
+++ imagemagick-jpeg-compress.cmake
@@ -0,0 +1,28 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/ImageMagick.cmake OPTIONAL RESULT_VARIABLE FFMPEG_IS_FOUND)
+if (FFMPEG_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+message("file name without .jpg extension")
+input(file)
+message(${file})
+
+message("percentage (100 is no compression)")
+input(percentage)
+message(${percentage})
+
+execute_process(
+	COMMAND convert "${file}" -quality ${percentage}% "${file}-compressed.jpg"
+	WORKING_DIRECTORY $ENV{USERPROFILE}/Downloads
+	OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+
+
+

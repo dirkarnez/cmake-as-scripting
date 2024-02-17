c387155fe18d07e6853dc997e1c24c7cc8d56ff1
dirkarnez <smalldirkalex@gmail.com>
2024-02-17 20:44:23 +0800

    - upload files

---
+++ ffmpeg-mux-video-audio.cmake
@@ -0,0 +1,24 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/ffmpeg.cmake OPTIONAL RESULT_VARIABLE FFMPEG_IS_FOUND)
+if (FFMPEG_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+message("video")
+input(video)
+message(${video})
+
+message("audio")
+input(audio)
+message(${audio})
+
+execute_process(
+	COMMAND ffmpeg -i "${video}" -i  "${audio}" -c copy output.mp4
+	WORKING_DIRECTORY $ENV{USERPROFILE}/Downloads
+	OUTPUT_STRIP_TRAILING_WHITESPACE)

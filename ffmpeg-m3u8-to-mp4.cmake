026f092bb174184af161d5c8346ac8adcacc6568
dirkarnez <smalldirkalex@gmail.com>
2024-03-05 03:48:38 +0800

    - minor

--- ffmpeg-m3u8-to-mp4.cmake
+++ ffmpeg-m3u8-to-mp4.cmake
@@ -4,12 +4,17 @@ if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
     # generate one
 endif()
 
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/ffmpeg.cmake OPTIONAL RESULT_VARIABLE FFMPEG_IS_FOUND)
+if (FFMPEG_IS_FOUND STREQUAL "NOTFOUND")
+	message("???")
+	# generate one
+endif()
+
 message("url")
 input(url)
 
 message("file name without .mp4 extension")
 input(file)
-
 
 execute_process(
 	COMMAND ffmpeg -i "${url}" -c copy "${file}.mp4"
--- ffmpeg-webm-to-m4a.cmake
+++ ffmpeg-webm-to-m4a.cmake
@@ -1,1 +1,24 @@
-ffmpeg -i input.webm -vn -c:a aac output.m4a
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/ffmpeg.cmake OPTIONAL RESULT_VARIABLE FFMPEG_IS_FOUND)
+if (FFMPEG_IS_FOUND STREQUAL "NOTFOUND")
+	message("???")
+	# generate one
+endif()
+
+message("file")
+input(file)
+
+# file(TO_CMAKE_PATH "${DIRECTORY}" DIRECTORY_NORMALIZED)
+# message(${file})
+
+get_filename_component(FILE_NAME_WITHOUT_EXTENSION ${file} NAME_WLE)
+
+execute_process(
+	COMMAND ffmpeg -i ${file} -vn -c:a aac "${FILE_NAME_WITHOUT_EXTENSION}-converted.m4a"
+	WORKING_DIRECTORY $ENV{USERPROFILE}/Downloads
+	OUTPUT_STRIP_TRAILING_WHITESPACE)

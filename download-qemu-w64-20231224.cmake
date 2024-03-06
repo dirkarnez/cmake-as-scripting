cb18400c31ac53cef392765f2c82bc28791b9d49
Dirk Arnez <smalldirkalex@gmail.com>
2024-03-06 22:33:34 +0800

    - add download-qemu-w64-20231224.cmake

---
+++ download-qemu-w64-20231224.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file_and_run("https://github.com/dirkarnez/qemu-portable/releases/download/20231224/qemu-w64-portable-20231224.zip")

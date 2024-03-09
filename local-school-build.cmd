372e8679ebb542b4b9bf69e700c696149e61b573
dirkarnez <smalldirkalex@gmail.com>
2024-03-10 00:43:22 +0800

    - disable Windows' curl

--- local-school-build.cmd
+++ local-school-build.cmd
@@ -8,7 +8,7 @@ SET PATH=^
 %DOWNLOADS_DIR%\PortableGit\bin;^
 %DOWNLOADS_DIR%\PortableGit\usr\bin;^
 %DOWNLOADS_DIR%\cmake-3.26.1-windows-x86_64\bin;^
-%SystemRoot%\System32;
+%DOWNLOADS_DIR%\curl-8.6.0_4-win64-mingw\curl-8.6.0_4-win64-mingw\bin;
 
 @REM set PYTHON_DIR=%DOWNLOADS_DIR%\python-3.10.8-amd64-portable
 @REM %DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^

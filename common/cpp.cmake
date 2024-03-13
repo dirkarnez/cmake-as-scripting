7730a93bd0e42553cddb4830d62ff33209509110
dirkarnez <smalldirkalex@gmail.com>
2024-03-13 20:07:38 +0800

    - add cpptools-win64.vsix installation

--- common/cpp.cmake
+++ common/cpp.cmake
@@ -2,7 +2,7 @@ set(suffix "_download_link")
 # set(1_17_5${suffix} "https://go.dev/dl/go1.17.5.windows-amd64.zip")
 # set(1_21_0${suffix} "https://go.dev/dl/go1.21.0.windows-amd64.zip")
 
-function(setup_cpp version)
+function(setup_cpp)
     set(download_url "")
 
     # if("${version}" STREQUAL "1.17.5")
@@ -23,7 +23,26 @@ function(setup_cpp version)
     #endif()
 
 
-	# .\bin\codium.cmd --install-extension="C:\Users\19081126D\Downloads\ms-vscode.cpptools-1.19.2@win32-x64.vsix"
+	# .
+	include(${CMAKE_CURRENT_SOURCE_DIR}/download-vscodium.cmake OPTIONAL RESULT_VARIABLE VSCODIUM_IS_FOUND)
+	if (VSCODIUM_IS_FOUND STREQUAL "NOTFOUND")
+		message("???")
+		# generate one
+	endif()
+	
+	if(EXISTS "$ENV{USERPROFILE}/Downloads/VSCodium-win32-x64-${TAG_NAME}/VSCodium.exe")
+		include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+		if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+			message("???")
+			# generate one
+		endif()
+
+		download_file("https://github.com/microsoft/vscode-cpptools/releases/download/v1.19.6/cpptools-win64.vsix")
+		
+		execute_process(
+			COMMAND codium.cmd --install-extension=$ENV{USERPROFILE}\\Downloads\\cpptools-win64.vsix
+			WORKING_DIRECTORY "$ENV{USERPROFILE}\\Downloads\\VSCodium-win32-x64-${TAG_NAME}\\bin")
+	endif()
     
     set(ENV{PATH} "$ENV{PATH};$ENV{USERPROFILE}\\Downloads\\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\\mingw64")
     set(ENV{PATH} "$ENV{PATH};$ENV{USERPROFILE}\\Downloads\\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\\mingw64\\bin")

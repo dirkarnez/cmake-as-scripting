2a86e857ff222afaa8483ddc25d588be50f2f4f9
Dirk Arnez <smalldirkalex@gmail.com>
2024-03-31 22:40:06 +0800

    - add download-mueditor-1.2.0.cmake

---
+++ download-mueditor-1.2.0.cmake
@@ -0,0 +1,8 @@
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file("https://github.com/mu-editor/mu/releases/download/v1.2.0/MuEditor-win64-1.2.0.msi")

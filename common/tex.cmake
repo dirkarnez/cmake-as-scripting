45f0d99639fc259f2a1e8088abeef73f9e6c5919
Dirk Arnez <smalldirkalex@gmail.com>
2024-04-26 05:53:13 +0800

    - common/tex.cmake

---
+++ common/tex.cmake
@@ -0,0 +1,2 @@
+https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_53822_64bit/strawberry-perl-5.38.2.2-64bit-portable.zip
+https://github.com/dirkarnez/miktex-portable/releases/download/v5.5.0.1763023/miktex-portable-v5.5.0.1763023.zip

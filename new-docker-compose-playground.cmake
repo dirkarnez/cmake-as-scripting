065f4d2ca50f96b0636cecf61356e8d04f2a7d85
Dirk Arnez <smalldirkalex@gmail.com>
2024-05-06 00:46:14 +0800

    - add new-docker-compose-playground.cmake

---
+++ new-docker-compose-playground.cmake
@@ -0,0 +1,8 @@
+include("common/github.cmake" OPTIONAL RESULT_VARIABLE GITHUB_IS_FOUND)
+
+if (GITHUB_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+new_repo("docker-compose-playground")

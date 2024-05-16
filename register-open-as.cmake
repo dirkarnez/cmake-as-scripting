6403856cb3a8f1d279f2ce8fbbcb49753fe739a6
dirkarnez <smalldirkalex@gmail.com>
2024-05-16 22:57:52 +0800

    - wip register-open-as.cmake

--- register-open-as.cmake
+++ register-open-as.cmake
@@ -1,24 +1,35 @@
-string(REPLACE "\\" "\\\\" CMAKE_USERPROFILE "$ENV{USERPROFILE}")
+execute_process(
+	COMMAND C:\\Windows\\System32\\whoami.exe
+	OUTPUT_VARIABLE CURRENT_USER
+	WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+	OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+
+if("${CURRENT_USER}" STREQUAL "administrator\\administrator")
+	string(REPLACE "\\" "\\\\" CMAKE_CMAKE_PATH "D:\\Softwares\\cmake-3.29.3-windows-x86_64\\bin\\cmake.exe")
+	string(REPLACE "\\" "\\\\" CMAKE_CMAKE_GUI_PATH "D:\\Softwares\\cmake-3.29.3-windows-x86_64\\bin\\cmake-gui.exe")
+else()
+	string(REPLACE "\\" "\\\\" CMAKE_CMAKE_PATH "$ENV{USERPROFILE}\\Downloads\\cmake-3.26.1-windows-x86_64\\bin\\cmake.exe")
+	string(REPLACE "\\" "\\\\" CMAKE_CMAKE_GUI_PATH "$ENV{USERPROFILE}\\Downloads\\cmake-3.26.1-windows-x86_64\\bin\\cmake-gui.exe")
+endif()
+
 
 configure_file("${CMAKE_CURRENT_SOURCE_DIR}/common/open-as.reg.in" "${CMAKE_CURRENT_SOURCE_DIR}/common/open-as.reg" @ONLY)
 
 execute_process(
 	COMMAND reg import open-as.reg
-	OUTPUT_VARIABLE CURRENT_USER
 	WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/common"
 )
 
-file(REMOVE "${CMAKE_CURRENT_SOURCE_DIR}/common/open-as.reg")
+# file(REMOVE "${CMAKE_CURRENT_SOURCE_DIR}/common/open-as.reg")
 
 execute_process(
 	COMMAND taskkill /f /im explorer.exe
-	OUTPUT_VARIABLE CURRENT_USER
 	WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/common"
 )
 	
 execute_process(
 	COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/common/exec-detached.bat
 	explorer.exe
-	OUTPUT_VARIABLE CURRENT_USER
 	WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/common"
 )

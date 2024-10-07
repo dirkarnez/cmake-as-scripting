8d357f7679ab0f49d88824ed625cc0362f78ab53
dirkarnez <smalldirkalex@gmail.com>
2024-10-07 08:08:54 +0800

    - add yt-dlp-best-ranged-with-ffmpeg.cmake

---
+++ yt-dlp-best-ranged-with-ffmpeg.cmake
@@ -0,0 +1,26 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/yt-dlp.cmake OPTIONAL RESULT_VARIABLE YT_DLP_IS_FOUND)
+if (YT_DLP_IS_FOUND STREQUAL "NOTFOUND")
+	message("???")
+	# generate one
+endif()
+
+message("file")
+input(file)
+
+message(${file})
+
+execute_process(
+	COMMAND yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" "${file}" --downloader-args "ffmpeg_i:-ss 1:36 -to 1:40"
+	WORKING_DIRECTORY $ENV{USERPROFILE}/Downloads
+	OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+
+
+	
+	

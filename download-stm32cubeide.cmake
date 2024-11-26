2442ab123dc49a9e10d70e72b935cd24576c7cfa
dirkarnez <smalldirkalex@gmail.com>
2024-11-26 21:13:36 +0800

    - add download-stm32cubeide.cmake

---
+++ download-stm32cubeide.cmake
@@ -0,0 +1,28 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file_and_uncompress(
+"https://github.com/dirkarnez/STM32CubeIDE-portable/releases/download/v1.6.1/STM32CubeIDE-portable-v1.6.1.zip.001"
+"https://github.com/dirkarnez/STM32CubeIDE-portable/releases/download/v1.6.1/STM32CubeIDE-portable-v1.6.1.zip.002"
+"https://github.com/dirkarnez/STM32CubeIDE-portable/releases/download/v1.6.1/STM32CubeIDE-portable-v1.6.1.zip.003"
+"https://github.com/dirkarnez/STM32CubeIDE-portable/releases/download/v1.6.1/STM32CubeIDE-portable-v1.6.1.zip.004"
+"https://github.com/dirkarnez/STM32CubeIDE-portable/releases/download/v1.6.1/STM32CubeIDE-portable-v1.6.1.zip.005"
+"https://github.com/dirkarnez/STM32CubeIDE-portable/releases/download/v1.6.1/STM32CubeIDE-portable-v1.6.1.zip.006"
+"https://github.com/dirkarnez/STM32CubeIDE-portable/releases/download/v1.6.1/STM32CubeIDE-portable-v1.6.1.zip.007"
+"https://github.com/dirkarnez/STM32CubeIDE-portable/releases/download/v1.6.1/STM32CubeIDE-portable-v1.6.1.zip.008"
+"https://github.com/dirkarnez/STM32CubeIDE-portable/releases/download/v1.6.1/STM32CubeIDE-portable-v1.6.1.zip.009"
+"https://github.com/dirkarnez/STM32CubeIDE-portable/releases/download/v1.6.1/STM32CubeIDE-portable-v1.6.1.zip.010"
+"https://github.com/dirkarnez/STM32CubeIDE-portable/releases/download/v1.6.1/STM32CubeIDE-portable-v1.6.1.zip.011"
+"https://github.com/dirkarnez/STM32CubeIDE-portable/releases/download/v1.6.1/STM32CubeIDE-portable-v1.6.1.zip.012"
+)
+
+# file(RENAME "$ENV{USERPROFILE}\\Downloads\\Keil_v5.38a_with_packages.zip" "$ENV{USERPROFILE}\\Downloads\\Keil_v5.38a_with_packages")
+
+# download_file_and_uncompress("https://github.com/dirkarnez/keil-mdk-portable-launcher/releases/download/v1.0.0/keil-mdk-portable-launcher-v1.0.0.zip")
+
+# file(RENAME "$ENV{USERPROFILE}\\Downloads\\keil-mdk-portable-launcher-v1.0.0\\UV4.Portable.exe" "$ENV{USERPROFILE}\\Downloads\\Keil_v5.38a_with_packages\\UV4.Portable.exe")
+
+# file(REMOVE_RECURSE "$ENV{USERPROFILE}\\Downloads\\keil-mdk-portable-launcher-v1.0.0")

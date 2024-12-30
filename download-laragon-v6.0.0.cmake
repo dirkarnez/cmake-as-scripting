e9b85c98bcc5bf58502a9e0f2ac464145f9c6de0
dirkarnez <smalldirkalex@gmail.com>
2024-12-30 17:28:35 +0800

    - add download-laragon-v6.0.0.cmake

---
+++ download-laragon-v6.0.0.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file_and_uncompress("https://github.com/dirkarnez/laragon-portable/releases/download/v6.0.0/laragon-php-8.0.0-mariadb-10.11.10-portable-v6.0.0.zip")

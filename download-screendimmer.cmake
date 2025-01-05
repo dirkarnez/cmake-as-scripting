bce351c5fc6cdfe39902164f52e41522250019e7
dirkarnez <smalldirkalex@gmail.com>
2025-01-06 02:35:13 +0800

    - add download-screendimmer.cmake

---
+++ download-screendimmer.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file_and_uncompress("https://github.com/dirkarnez/ScreenDimmer/releases/download/v1.0.0/ScreenDimmer-v1.0.0-winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1.zip")

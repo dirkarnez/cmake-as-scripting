acd08eb8b971b63786d9f83506b76924579623ea
dirkarnez <smalldirkalex@gmail.com>
2025-01-18 03:28:20 +0800

    - update ffmpeg-mp4.cmake

--- common/ffmpeg.cmake
+++ common/ffmpeg.cmake
@@ -1,3 +1,9 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/env.cmake OPTIONAL RESULT_VARIABLE ENV_IS_FOUND)
+if (ENV_IS_FOUND STREQUAL "NOTFOUND")
+	message("???")
+	# generate one
+endif()
+
 if(NOT EXISTS "$ENV{SOFTWARE_DIR}/ffmpeg-6.0-full_build-shared/ffmpeg-6.0-full_build-shared/bin/ffmpeg.exe")
 	message("downloading ffmpeg")
 	download_file_and_uncompress("https://github.com/GyanD/codexffmpeg/releases/download/6.0/ffmpeg-6.0-full_build-shared.zip")
--- ffmpeg-mp4.cmake
+++ ffmpeg-mp4.cmake
@@ -18,7 +18,7 @@ input(file)
 
 get_filename_component(FILE_NAME_WITHOUT_EXTENSION ${file} NAME_WLE)
 
-set(ENV{PATH} "$ENV{USERPROFILE}\\Downloads\\ffmpeg-6.0-full_build-shared\\ffmpeg-6.0-full_build-shared\\bin")
+set(ENV{PATH} "$ENV{SOFTWARE_DIR}\\ffmpeg-6.0-full_build-shared\\ffmpeg-6.0-full_build-shared\\bin")
 
 execute_process(
 	COMMAND ffmpeg -i "${file}" "${FILE_NAME_WITHOUT_EXTENSION}-converted.mp4"

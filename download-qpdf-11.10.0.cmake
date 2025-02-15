476c90784d5e470eaa47e4608275c74b81957c8c
dirkarnez <smalldirkalex@gmail.com>
2025-02-15 23:24:09 +0800

    - update download-qpdf-11.10.0.cmake

--- download-qpdf-11.10.0.cmake
+++ download-qpdf-11.10.0.cmake
@@ -5,6 +5,7 @@ if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
 endif()
 
 message("%SOFTWARE_DIR%\\qpdf-11.10.0-mingw64\\bin\\qpdf.exe --decrypt --password=\"Original Password\" input.pdf output.pdf")
+message("%SOFTWARE_DIR%\\qpdf-11.10.0-mingw64\\bin\\qpdf.exe --linearize input.pdf output.pdf")
 
 if(NOT EXISTS "$ENV{USERPROFILE}/Downloads/qpdf-11.10.0-mingw64/bin/qpdf.exe")
 	download_file_and_uncompress("https://github.com/qpdf/qpdf/releases/download/v11.10.0/qpdf-11.10.0-mingw64.zip")

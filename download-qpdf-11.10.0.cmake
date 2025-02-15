b9a38a3410a2546d25cfe8db3cbf6e29b0eadc64
dirkarnez <smalldirkalex@gmail.com>
2025-02-15 22:24:44 +0800

    - add download-qpdf-11.10.0.cmake

---
+++ download-qpdf-11.10.0.cmake
@@ -0,0 +1,11 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+if(NOT EXISTS "$ENV{USERPROFILE}/Downloads/qpdf-11.10.0-mingw64/bin/qpdf.exe")
+	download_file_and_uncompress("https://github.com/qpdf/qpdf/releases/download/v11.10.0/qpdf-11.10.0-mingw64.zip")
+else()
+	message("qpdf.exe exists")
+endif()

9d7f83e8e3e4f1745990f8b8369fd5cb8d427cb5
dirkarnez <smalldirkalex@gmail.com>
2025-03-03 01:47:54 +0800

    - add download-handbrake.cmake

---
+++ download-handbrake.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file_and_uncompress("https://github.com/HandBrake/HandBrake/releases/download/1.3.0/HandBrake-1.3.0-x86_64-Win_GUI.zip")

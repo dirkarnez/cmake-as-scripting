b73e80280e664884c67a3ceece876f180448a0f9
dirkarnez <smalldirkalex@gmail.com>
2025-04-08 20:25:46 +0800

    - add download-file-searcher-v1.0.0.cmake

---
+++ download-file-searcher-v1.0.0.cmake
@@ -0,0 +1,9 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file_and_uncompress("https://github.com/dirkarnez/file-searcher/releases/download/v1.0.0/file-searcher-v1.0.0-windows-latest.zip")
+
+

2da84472ef650faf628716e3fd7ff4fa1178bce5
dirkarnez <smalldirkalex@gmail.com>
2025-04-09 21:26:25 +0800

    - add download-jdk-24.cmake

---
+++ download-jdk-24.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file_and_uncompress("https://github.com/adoptium/temurin24-binaries/releases/download/jdk-24%2B36/OpenJDK24U-jdk_x64_windows_hotspot_24_36.zip")

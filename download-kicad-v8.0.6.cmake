5a65a1c10f95466125257ce06d18daf25a18e1f2
dirkarnez <smalldirkalex@gmail.com>
2025-04-13 00:06:38 +0800

    - add download-kicad-v8.0.6.cmake

---
+++ download-kicad-v8.0.6.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file_and_uncompress("https://github.com/dirkarnez/kicad-portable/releases/download/v8.0.6/kicad-portable-v8.0.6.zip")

f8bd2d05c2221a3196768ef2c436454c261b9a28
dirkarnez <smalldirkalex@gmail.com>
2025-08-17 23:19:00 +0800

    - add aria2-torrent.cmake

---
+++ aria2-torrent.cmake
@@ -0,0 +1,22 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/aria2.cmake OPTIONAL RESULT_VARIABLE ARIA2_IS_FOUND)
+if (ARIA2_IS_FOUND STREQUAL "NOTFOUND")
+	message("???")
+	# generate one
+endif()
+
+# TODO add torrent file later
+
+message("magnet link")
+input(magnet_link)
+message(${magnet_link})
+
+execute_process(
+	COMMAND aria2c --seed-time=0 ${magnet_link} -d "aria2c-downloads"  
+	WORKING_DIRECTORY $ENV{USERPROFILE}/Downloads
+	OUTPUT_STRIP_TRAILING_WHITESPACE)

688127367bd48ea47ba0c1b0a9aa841031a064db
dirkarnez <smalldirkalex@gmail.com>
2025-08-27 00:09:22 +0800

    - add sync.cmake, wip

---
+++ sync.cmake
@@ -0,0 +1,12 @@
+include(${CMAKE_CURRENT_LIST_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "utils.cmake not available")
+    message(FATAL_ERROR "???")
+    # generate one
+endif()
+
+message("Directory")
+input(DIRECTORY)
+message("${DIRECTORY}")
+
+
+file(RENAME <oldname> <newname>)

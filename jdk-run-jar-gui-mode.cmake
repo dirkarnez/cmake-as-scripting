3aa1cba28b9c40ba546feef14cd442a16a634bb2
dirkarnez <smalldirkalex@gmail.com>
2025-10-07 03:49:41 +0800

    - add jdk-run-jar-gui-mode.cmake

---
+++ jdk-run-jar-gui-mode.cmake
@@ -0,0 +1,32 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/env.cmake OPTIONAL RESULT_VARIABLE ENV_IS_FOUND)
+if (ENV_IS_FOUND STREQUAL "NOTFOUND")
+	message("???")
+	# generate one
+endif()
+
+include(${CMAKE_CURRENT_LIST_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOT_FOUND")
+    message(FATAL_ERROR "???")
+    # generate one
+endif()
+
+
+set(ENV{PATH} "$ENV{SOFTWARE_DIR}\\OpenJDK8U-jdk_x64_windows_hotspot_8u382b05\\jdk8u382-b05\\jre\\bin")
+
+message("$ENV{PATH}")
+
+message("JAR path")
+input(JAR_PATH)
+message("${JAR_PATH}")
+
+# if("${DIRECTORY}" STREQUAL ".")
+# 	set(DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
+# endif()
+
+
+execute_process(COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/common/exec.bat 
+  javaw.exe 
+  -jar 
+  ${JAR_PATH}
+  WORKING_DIRECTORY "$ENV{USERPROFILE}/Downloads"
+)

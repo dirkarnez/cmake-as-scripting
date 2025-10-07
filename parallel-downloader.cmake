7cf4ffe6898843320539e8555f98f48ad296c29e
dirkarnez <smalldirkalex@gmail.com>
2025-10-07 21:30:32 +0800

    - wip parallel-downloader.cmake

--- parallel-downloader.cmake
+++ parallel-downloader.cmake
@@ -1,3 +1,7 @@
-curl --range 0-999999 -o part1.zip https://example.com/largefile.zip
-curl --range 1000000- -o part2.zip https://example.com/largefile.zip
-# Then combine part1.zip and part2.zip
+curl -I https://tunnelsassetsprod.blob.core.windows.net/cli/devtunnel.exe | grep -i Content-Length
+curl -I https://tunnelsassetsprod.blob.core.windows.net/cli/devtunnel.exe | grep -i Accept-Ranges
+22592216
+curl --range 0-999999 -o part1 https://tunnelsassetsprod.blob.core.windows.net/cli/devtunnel.exe
+curl --range 1000000-22592215 -o part2 https://tunnelsassetsprod.blob.core.windows.net/cli/devtunnel.exe
+
+cat part1 part2 > target_file

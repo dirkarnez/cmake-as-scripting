967c2a2e70c3c7b5f2b13e14131a1b5582a58585
dirkarnez <smalldirkalex@gmail.com>
2025-10-12 02:48:07 +0800

    - add download-klayout.cmake

---
+++ download-klayout.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file_and_uncompress("https://www.klayout.org/downloads/Windows/klayout-0.30.4-win64.zip")

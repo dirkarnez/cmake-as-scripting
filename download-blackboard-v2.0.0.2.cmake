72c257087bc09a1a550ab9f29a355f381a3295f5
dirkarnez <smalldirkalex@gmail.com>
2025-10-14 07:21:19 +0800

    - add download-blackboard-v2.0.0.2.cmake

---
+++ download-blackboard-v2.0.0.2.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file_and_uncompress("https://github.com/mpue/blackboard/releases/download/2.0.0.2/blackboard-bin-2.0.0.2.zip")

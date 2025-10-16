04618f5cfc35429b6d1199adc077fca430a7f557
dirkarnez <smalldirkalex@gmail.com>
2025-10-17 01:51:32 +0800

    - update download-qt-studio.cmake

--- download-qt-studio.cmake
+++ download-qt-studio.cmake
@@ -4,4 +4,5 @@ if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
     # generate one
 endif()
 
-download_file("https://download.qt.io/official_releases/qtcreator/17.0/17.0.2/qt-creator-opensource-windows-x86_64-17.0.2.exe")
+# download_file("https://download.qt.io/official_releases/qtcreator/17.0/17.0.2/qt-creator-opensource-windows-x86_64-17.0.2.exe")
+download_file_and_uncompress("https://ftp.jaist.ac.jp/pub/qtproject/official_releases/qtcreator/17.0/17.0.2/installer_source/windows_x64/qtcreator.7z")

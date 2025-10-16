683bd35430237cd881347badbc2104854258a3bc
dirkarnez <smalldirkalex@gmail.com>
2025-10-17 01:45:00 +0800

    - add download-qt-studio.cmake

---
+++ download-qt-studio.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_SOURCE_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file("https://download.qt.io/official_releases/qtcreator/17.0/17.0.2/qt-creator-opensource-windows-x86_64-17.0.2.exe")

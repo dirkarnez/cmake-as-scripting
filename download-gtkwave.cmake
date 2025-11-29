815e3a7fc5190076301e585350c41b7dfded000d
dirkarnez <smalldirkalex@gmail.com>
2025-11-29 21:27:24 +0800

    - add download-gtkwave.cmake

---
+++ download-gtkwave.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_LIST_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+download_file("https://github.com/gtkwave/gtkwave/releases/download/nightly/gtkwave_gtk3_mingw64_standalone.tgz")

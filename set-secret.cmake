60c4f1be0b5f192ab681eadab9fe0e17abc4182c
dirkarnez <smalldirkalex@gmail.com>
2025-12-08 11:04:28 +0800

    - update set-secret.cmake

--- set-secret.cmake
+++ set-secret.cmake
@@ -1,5 +1,9 @@
 string(REPLACE "\\" "\\\\" CMAKE_GIT_TOKEN "$ENV{GIT_TOKEN}")
 
+if("${CMAKE_GIT_TOKEN}" STREQUAL "")
+    message(STATUS "CMAKE_GIT_TOKEN is empty or undefined.")
+endif()
+
 configure_file("${CMAKE_CURRENT_LIST_DIR}/common/secrets.cmake.in" "${CMAKE_CURRENT_LIST_DIR}/common/secrets.cmake" @ONLY)
 
 file(REMOVE "${CMAKE_CURRENT_LIST_DIR}/common/open-as.reg")

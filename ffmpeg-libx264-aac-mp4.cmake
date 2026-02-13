5c7e7fa8778d0278c8e259026cb201a0ca6708a3
dirkarnez <smalldirkalex@gmail.com>
2026-02-13 23:28:12 +0800

    - update ffmpeg-libx264-aac-mp4.cmake

--- ffmpeg-libx264-aac-mp4.cmake
+++ ffmpeg-libx264-aac-mp4.cmake
@@ -22,7 +22,7 @@ message("${crf}")
 
 get_filename_component(FILE_NAME_WITHOUT_EXTENSION ${file} NAME_WLE)
 
-set(ENV{PATH} "$ENV{USERPROFILE}\\Downloads\\ffmpeg-6.0-full_build-shared\\ffmpeg-6.0-full_build-shared\\bin")
+set(ENV{PATH} "$ENV{SOFTWARE_DIR}\\ffmpeg-6.0-full_build-shared\\ffmpeg-6.0-full_build-shared\\bin")
 
 execute_process(
 	COMMAND ffmpeg -i "${file}" -c:v libx264 -crf ${crf} -c:a aac "${FILE_NAME_WITHOUT_EXTENSION}-converted.mp4"

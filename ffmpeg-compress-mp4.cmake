82716b29495dba45465fd69196f081db093b1a98
dirkarnez <smalldirkalex@gmail.com>
2026-04-16 01:40:35 +0800

    - add ffmpeg-compress-mp4.cmake

---
+++ ffmpeg-compress-mp4.cmake
@@ -0,0 +1,32 @@
+include(${CMAKE_CURRENT_LIST_DIR}/common/utils.cmake OPTIONAL RESULT_VARIABLE UTILS_IS_FOUND)
+if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
+    message("???")
+    # generate one
+endif()
+
+include(${CMAKE_CURRENT_LIST_DIR}/common/ffmpeg.cmake OPTIONAL RESULT_VARIABLE FFMPEG_IS_FOUND)
+if (FFMPEG_IS_FOUND STREQUAL "NOTFOUND")
+	message("???")
+	# generate one
+endif()
+
+message("file")
+input(file)
+
+# file(TO_CMAKE_PATH "${DIRECTORY}" DIRECTORY_NORMALIZED)
+# message("${file}"")
+
+get_filename_component(FILE_NAME_WITHOUT_EXTENSION ${file} NAME_WLE)
+
+message("crf value (0 - 51, 0 is lossless, 23 is the default)")
+input(crf)
+message("${crf}")
+
+execute_process(
+ 	COMMAND ffmpeg -i "${file}" -crf ${crf} "${FILE_NAME_WITHOUT_EXTENSION}-converted.mp4"
+	WORKING_DIRECTORY $ENV{USERPROFILE}/Downloads
+	OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+
+
+

6e26c82964f46a1e620315a2ce9a0dc63849402d
dirkarnez <smalldirkalex@gmail.com>
2026-05-20 12:26:27 +0800

    - update download-sharedown.cmake

--- download-sharedown.cmake
+++ download-sharedown.cmake
@@ -4,5 +4,7 @@ if (UTILS_IS_FOUND STREQUAL "NOTFOUND")
     # generate one
 endif()
 
-download_file_and_uncompress("https://github.com/dirkarnez/sharedown-portable/releases/download/v5.3.6/sharedown-v5.3.6-portable.zip")
+# download_file_and_uncompress("https://github.com/dirkarnez/sharedown-portable/releases/download/v5.3.6/sharedown-v5.3.6-portable.zip")
+download_file_and_uncompress("https://github.com/dirkarnez/sharedown-portable/releases/download/cf3d587cc3e24f7ff8fe4732e1465cb92eb420be3/sharedown-cf3d587cc3e24f7ff8fe4732e1465cb92eb420be3-portable.zip")
+
 
